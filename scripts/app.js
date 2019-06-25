c34125cb4ddc1b27910e225ad4d474fc57e00502
Charisse Strong <msstrong92@gmail.com>
2019-06-24 20:02:22 -0400

    carousel added

---
+++ scripts/app.js
@@ -0,0 +1,22 @@
+let slideIndex = 0;
+
+let showDivs = (n)=> {
+	let x = document.getElementsByClassName("image");
+	if (n > x.length -1) {
+		slideIndex = 0;
+	}
+	if (n < 0) {
+		slideIndex = x.length -1;
+	}
+	let i = 0;
+	while (i < x.length){
+		x[i].style.display = "none";
+		i++;
+	};
+	x[slideIndex].style.display = "block";
+};
+
+let plusDivs = (n)=> {
+	showDivs(slideIndex += n);
+};
+showDivs(slideIndex);
